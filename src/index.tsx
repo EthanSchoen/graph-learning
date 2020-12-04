9f40f1e11fc0f1d1bb7aba354b8f8117bfac4035
EthanSchoen <ethascho@gmail.com>
2020-12-04 10:05:36 -0500

    init commit

---
+++ src/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const Application = () => {
+  return <h1>Hello buddy</h1>;
+};
+
+ReactDOM.render(<Application />, document.getElementById('root'));
